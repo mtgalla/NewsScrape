7152fca7cb6638b60e54f7a9ab7aa411bc942d6d
mtgalla <mtgalla@gmail.com>
2019-06-01 15:11:14 -0500

    add get route

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 // Routes
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
